21a50f518c916b59f7b2080ddcd660fb8ced546f
Nigel.Zheng <nigel.zheng@emerson.com>
2019-04-04 15:21:48 +0800

    feat: enable ModelBuilderPlugin, translate api model desc

--- src/main/java/io/pluto/springfox/swagger/ApiModelLookup.java
+++ src/main/java/io/pluto/springfox/swagger/ApiModelLookup.java
@@ -2,19 +2,25 @@ package io.pluto.springfox.swagger;
 
 import com.fasterxml.classmate.TypeResolver;
 import io.swagger.annotations.ApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.schema.ModelBuilderPlugin;
 import springfox.documentation.spi.schema.contexts.ModelContext;
 import springfox.documentation.spring.web.DescriptionResolver;
 
+import static springfox.documentation.swagger.common.SwaggerPluginSupport.SWAGGER_PLUGIN_ORDER;
 import static springfox.documentation.swagger.common.SwaggerPluginSupport.pluginDoesApply;
 
+@Component
+@Order(value = SWAGGER_PLUGIN_ORDER + 1000)
 public class ApiModelLookup implements ModelBuilderPlugin {
   private final TypeResolver typeResolver;
   private final DescriptionResolver descriptions;
 
-
+  @Autowired
   public ApiModelLookup(TypeResolver typeResolver, DescriptionResolver descriptionResolver) {
     this.typeResolver = typeResolver;
     this.descriptions = descriptionResolver;
@@ -31,7 +37,6 @@ public class ApiModelLookup implements ModelBuilderPlugin {
   private Class<?> forClass(ModelContext context) {
     return typeResolver.resolve(context.getType()).getErasedType();
   }
-
 
   @Override
   public boolean supports(DocumentationType delimiter) {
